cd613776bb7209923a22a5d9159946e38f1de746
leonardo-gallegos <renzogallegos96@gmail.com>
2023-03-22 23:49:57 -0500

    Used querySelector

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+//ReactDOM.render(<h1>Leonardo edited this!</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>This is different!</h1>, document.querySelector("#root"))
